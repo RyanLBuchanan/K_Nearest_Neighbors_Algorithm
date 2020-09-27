90558f56d8396025a8c72b4017ff3699536c0638
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-26 18:21:59 -0600

    Project setup

---
+++ K_Nearest_Neighbors_Algorithm.py
@@ -0,0 +1,7 @@
+# K-Nearest Neighbors Algorithm tutorial from Machine Learning A-Z - SuperDataScience
+# Input by Ryan L Buchanan 26SEP20
+
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
