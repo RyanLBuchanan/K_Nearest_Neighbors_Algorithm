d596f12e76ac7a5dc0d9959a321682b14fa57f6f
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-26 20:39:26 -0600

    Load dataset & split

--- K_Nearest_Neighbors_Algorithm.py
+++ K_Nearest_Neighbors_Algorithm.py
@@ -5,3 +5,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Import dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, 2:4].values
+y = dataset.iloc[:, -1].values
+
+# Split the dataset into Training & Test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, Y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+print(X_train)
+
+print(X_test)
+
+print(y_train)
+# Train the K-NN model on the Training set
+from sklearn.neighbors import KNeighborsClassifier
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X, y)
