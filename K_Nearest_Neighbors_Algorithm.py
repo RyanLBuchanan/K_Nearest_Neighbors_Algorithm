e7e64d67ca30871061ef44bba4ea30986808dfff
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-02 05:37:12 -0600

    Train KNN model

--- K_Nearest_Neighbors_Algorithm.py
+++ K_Nearest_Neighbors_Algorithm.py
@@ -39,6 +39,6 @@ print(X_test)
 
 # Train the K-NN model on the Training set
 from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors=3)
-classifier.fit(X, y)
+classifier = KNeighborsClassifier(n_neighbors=5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
 
